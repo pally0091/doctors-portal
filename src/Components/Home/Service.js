f48f773de595a48b01478fe4da0309cfdd3de419
Debashis Roy <goodquote39@hotmail.com>
2022-11-15 18:18:11 +0600

    service card done

--- src/Components/Home/Service.js
+++ src/Components/Home/Service.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const Service = () => {
+const Service = ({service}) => {
+    const {name, description, logo} = service
     return (
-        <div>
-            
+        <div className='text-center shadow-lg shadow-black w-[30%] p-5 rounded-md'>
+            <div className='w-24 mx-auto'>
+            <img src={logo} alt=''/>
+            </div>
+            <h3 className='font-bold text-lg mt-2'>{name}</h3>
+            <p className='text-justify mt-2'>{description}</p>
         </div>
     );
 };
--- src/Components/Home/Services.js
+++ src/Components/Home/Services.js
@@ -1,14 +1,49 @@
-import React from 'react';
+import React from "react";
+import fluoride from "../../assets/images/fluoride.png";
+import cavity from "../../assets/images/cavity.png";
+import whitening from "../../assets/images/whitening.png";
+import Service from "./Service";
 
 const Services = () => {
-    return (
-        <div className='mt-10'>
-            <div>
-                <p className='text-[#19D3AE]'>OUR SERVICES</p>
-                <h3>Services We Provide</h3>
-            </div>
+  const servicedata = [
+    {
+      name: "Fluoride Treatment",
+      description:
+        "Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the",
+      logo: fluoride,
+    },
+    {
+      name: "Cavity Filling",
+      description:
+        "Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the",
+      logo: cavity,
+    },
+    {
+      name: "Teeth Whitening",
+      description:
+        "Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the",
+      logo: whitening,
+    },
+  ];
+  return (
+    <div className="mt-10">
+      <div>
+        <p className="text-[#19D3AE]">OUR SERVICES</p>
+        <h3>Services We Provide</h3>
+        <div className="flex justify-evenly mt-5">
+          {servicedata.map((service) => (
+            <Service
+              key={service.name}
+              service={service}
+            ></Service>
+          ))}
         </div>
-    );
+        <div className="mt-10">
+            service banner
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Services;
