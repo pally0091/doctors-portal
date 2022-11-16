104f3114c2db367b0ad11db05864ea968e169da2
Debashis Roy <goodquote39@hotmail.com>
2022-11-16 11:43:12 +0600

    service div complete

--- src/Components/Home/Services.js
+++ src/Components/Home/Services.js
@@ -3,6 +3,7 @@ import fluoride from "../../assets/images/fluoride.png";
 import cavity from "../../assets/images/cavity.png";
 import whitening from "../../assets/images/whitening.png";
 import Service from "./Service";
+import dental from '../../assets/images/treatment.png';
 
 const Services = () => {
   const servicedata = [
@@ -38,8 +39,14 @@ const Services = () => {
             ></Service>
           ))}
         </div>
-        <div className="mt-10">
-            service banner
+        <div className="mt-10 w-11/12 mx-auto flex items-center shadow-md shadow-slate-500">
+            <div className="w-1/2 h-96 px-10 ">
+              <img className="w-full h-full rounded-md" src={dental} alt=''/>
+              </div>
+              <div className="w-1/2 px-10">
+                <h2 className="text-3xl font-semibold">Exceptional Dental Care, on Your Terms</h2>
+                <p className="text-justify mt-5">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
+              </div>
         </div>
       </div>
     </div>
